09cfc073b9c1d3ce99a50ff7bdb7aa6fe5bc8c51
Buddhima Kudagama <buddhima.kudagama@agileinnovationsgroup.com>
2019-01-07 14:58:32 +0530

    Gradient stack layout renderer added for iOS project

---
+++ App1/App1/App1.iOS/Renderers/GradientColorStackRendererAdvanced.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using App1.Renderers;
+using mode = App1.Renderers.GradientColorStackModes;
+using Xamarin.Forms;
+using App1.iOS.Renderers;
+using Xamarin.Forms.Platform.iOS;
+using CoreGraphics;
+using CoreAnimation;
+
+[assembly: ExportRenderer(typeof(GradientColorStackAdvanced), typeof(GradientColorStackRendererAdvanced))]
+namespace App1.iOS.Renderers
+{
+    public class GradientColorStackRendererAdvanced : VisualElementRenderer<StackLayout>
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            GradientColorStackAdvanced layout = (GradientColorStackAdvanced)Element;
+
+            CGColor[] colors = new CGColor[layout.Colors.Length];
+
+            for (int i = 0, l = colors.Length; i < l; i++)
+            {
+                colors[i] = layout.Colors[i].ToCGColor();
+            }
+
+            var gradientLayer = new CAGradientLayer();
+
+            switch (layout.Mode)
+            {
+                default:
+                case mode.GradientColorStackMode.ToRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 0.5);
+                    gradientLayer.EndPoint = new CGPoint(1, 0.5);
+                    break;
+                case mode.GradientColorStackMode.ToLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 0.5);
+                    gradientLayer.EndPoint = new CGPoint(0, 0.5);
+                    break;
+                case mode.GradientColorStackMode.ToTop:
+                    gradientLayer.StartPoint = new CGPoint(0.5, 0);
+                    gradientLayer.EndPoint = new CGPoint(0.5, 1);
+                    break;
+                case mode.GradientColorStackMode.ToBottom:
+                    gradientLayer.StartPoint = new CGPoint(0.5, 1);
+                    gradientLayer.EndPoint = new CGPoint(0.5, 0);
+                    break;
+                case mode.GradientColorStackMode.ToTopLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 0);
+                    gradientLayer.EndPoint = new CGPoint(0, 1);
+                    break;
+                case mode.GradientColorStackMode.ToTopRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 1);
+                    gradientLayer.EndPoint = new CGPoint(1, 0);
+                    break;
+                case mode.GradientColorStackMode.ToBottomLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 1);
+                    gradientLayer.EndPoint = new CGPoint(0, 0);
+                    break;
+                case mode.GradientColorStackMode.ToBottomRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 0);
+                    gradientLayer.EndPoint = new CGPoint(1, 1);
+                    break;
+            }
+
+            gradientLayer.Frame = rect;
+            gradientLayer.Colors = colors;
+
+            NativeView.Layer.InsertSublayer(gradientLayer, 0);
+        }
+    }
+}
